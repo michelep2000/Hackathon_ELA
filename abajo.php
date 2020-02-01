9edbbc49eca054f0cda536761a48bf48dd01104f
Alfredo <manjon.canela@gmail.com>
2020-02-01 17:06:18 +0100

    cambios login

--- abajo.php
+++ abajo.php
@@ -1,14 +1,23 @@
-<br>
-<footer class="text-light bg-dark w-100 mt-5">
-<div class="footer-copyright text-center py-3">© 2020 Copyright:
-    <a href="./index.php"> Grupo2 Hacking humanity Alfredo Manjón Jorgue Caro Michele Pardo Jaime Casado Beatriz López</a>
-  </div>
-</footer>
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<footer class="text-center bg-light footer">
+      <div class="footer-above">
+        <div class="container">
+          <div class="row d-flex justify-content-center">
+            <div class="footer-col ">
+              <a class="navbar-brand" href="index.php"><img src="img/logo1.png" width="210" class="logo2" /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-below">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              Copyright &copy; Grupo2
+            </div>
+            <br>
+          </div>
+        </div>
+      </div>
+    </footer>
 </body>
 </html>
